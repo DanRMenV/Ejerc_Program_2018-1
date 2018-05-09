316926291428eee37ffdfeb5a8b1d294f032097c
DanRMenV <37118946+DanRMenV@users.noreply.github.com>
2018-05-09 18:44:57 -0500

    Update geometricos.cpp

--- Ejercicios/geometricos.cpp
+++ Ejercicios/geometricos.cpp
@@ -2,13 +2,6 @@
 #include <iostream>
 using namespace std;
 
-void escribir_arreglo_double(double* X){
-    for(int i=0;i<2;i++){
-        cout<<X[i]<<endl;
-        }
-
-}
-
 char* par_per(double p1, double c1, double p2, double c2){
 
     if(p1 == p2){
@@ -23,16 +16,51 @@ char* par_per(double p1, double c1, double p2, double c2){
 }
 
 double* inter(double p1, double c1, double p2, double c2){
-
     double x=(c2-c1)/(p1-p2);
     double y= p1*x+c1;
-
     double* r= new double [2];
     r[0]= x;
     r[1]= y;
-
     return r;
-
 }
 
 
+double ar_cu_ins(int r){
+    int d=2*r;
+
+    return potencia(d,2)/2;
+}
+
+double per_cu_ins(int r){
+    int d=2*r;
+    int per= potencia(d,2)/2;
+
+    return 4*raiz(per);
+}
+
+double ar_pe_ins(int r){
+
+    double h=raiz((r*r)-((r/2)*(r/2)));
+    double t=r*h/2;
+
+    return 5*t;
+}
+
+double per_pe_ins(int r){
+
+    return 5*r;
+}
+
+
+double ar_hx_ins(int r){
+
+    double h=raiz((r*r)-((r/2)*(r/2)));
+    double t=r*h/2;
+
+    return 6*t;
+}
+
+double per_hx_ins(int r){
+
+    return 6*r;
+}
