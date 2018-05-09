8e64f15008f53150bd363822eab25ab899c959ad
DanRMenV <37118946+DanRMenV@users.noreply.github.com>
2018-05-09 18:43:37 -0500

    Update geometricos.h

--- Ejercicios/geometricos.h
+++ Ejercicios/geometricos.h
@@ -5,5 +5,11 @@ void escribir_arreglo_double(double* X);
 char* par_per(double p1, double c1, double p2, double c2);
 double* inter(double p1, double c1, double p2, double c2);
 
+double ar_cu_ins(int r);
+double per_cu_ins(int r);
+double ar_pe_ins(int r);
+double per_pe_ins(int r);
+double ar_hx_ins(int r);
+double per_hx_ins(int r);
 
 #endif // GEOMETRICOS_H_INCLUDED
