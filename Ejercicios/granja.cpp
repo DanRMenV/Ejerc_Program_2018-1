3ba79635d4a9f31fc12e615eb136c8fc3d263c15
DanRMenV <37118946+DanRMenV@users.noreply.github.com>
2018-05-09 19:03:41 -0500

    Add files via upload

---
+++ Ejercicios/granja.cpp
@@ -0,0 +1,39 @@
+#include "granja.h"
+#include "numericos.h"
+
+double leche( int v, double n, double m, double M, double x ){
+
+    return (n*m*x)/(v*M);
+
+}
+
+int huevos (int A){
+    int G = A/3;
+    return 8*G;
+
+}
+
+int escorpiones( int ep, int em, int eg ){
+    int pes = 20*ep + 30*em + 50*eg ;
+    int pob_i = ep + em + eg;
+    int pob_f = (2/3)* pob_i;
+
+
+}
+
+char* c_corral(int p, int q, int s, int m, int n){
+    int pm = 2*4*q*(m+n);
+    int pa = 2*5*p*(m+n);
+    int pv = 2*8*s*(m+n);
+
+    if(pm<pa && pm<pv){
+        return "Madera";
+    }else{
+        if(pa<pv){
+            return "Alambre";
+        }
+        else{
+            return "Varilla";
+        }
+    }
+}
---
+++ Ejercicios/granja.h
@@ -0,0 +1,8 @@
+#ifndef GRANJA_H_INCLUDED
+#define GRANJA_H_INCLUDED
+
+double leche(int v, double n, double m, double M, double x);
+int huevos (int A);
+char* c_corral(int p, int q, int s, int m, int n);
+
+#endif // GRANJA_H_INCLUDED
