af43815be2e203c05cd03514c9a71017d6ef84fc
DanRMenV <37118946+DanRMenV@users.noreply.github.com>
2018-05-15 19:17:33 -0500

    Create polinomios.cpp

---
+++ polinomios.cpp
@@ -0,0 +1,82 @@
+void escribir_arreglo_int(int* X, int n){
+    for(int i=0;i<n;i++){
+        cout<<X[i]<<",";
+    };
+}
+
+double* crear_arreglo_pol(int n){
+    return new double[n+1];
+};
+
+double* leer_arreglo_pol(double* x, int n){
+    for(int i = 0; i <= n; i++){
+        cout<<"Ingrese dato x^"<<n-i<<":";
+        cin>>x[i];
+    };
+    return x;
+};
+
+void escribir_arreglo_sum(double* X, int n){
+    for(int i=0;i<n;i++){
+        cout<<"("<<X[i]<<"x^"<<n-i<<")+";
+    }
+    cout<<"("<<X[n]<<")";
+}
+
+double evaluar_pol(double* A, double x, int n, int e ){
+    if(n==0){
+        return A[0]*potencia(x,e);
+    }else{
+        return evaluar_pol(A,x,n-1,e+1)+ A[n]*potencia(x,e);
+    };
+};
+
+double* sum_pol(double* A, double* B, int n){
+    if(n==0){
+        A[0]= A[0]+B[0];
+        return A;
+    }else{
+        A[n]=A[n]+B[n];
+        return sum_pol(A,B,n-1);
+    }
+}
+
+double* rest_pol(double* A, double* B, int n){
+    if(n==0){
+        A[0]= A[0]-B[0];
+        return A;
+    }else{
+        A[n]=A[n]-B[n];
+        return rest_pol(A,B,n-1);
+    }
+}
+
+double* mult_pol(double* A, double* B, int n){
+    if(n==0){
+        A[0]= A[0]*B[0];
+        return A;
+    }else{
+        A[n]=A[n]*B[n];
+        return mult_pol(A,B,n-1);
+    }
+}
+
+double* div_pol(double* A, double* B, int n){
+    if(n==0){
+        A[0]= A[0]/B[0];
+        return A;
+    }else{
+        A[n]=A[n]/B[n];
+        return div_pol(A,B,n-1);
+    }
+}
+
+double* resid_pol(double* A, double* B, int n){
+    if(n==0){
+        A[0]= A[0]%B[0];
+        return A;
+    }else{
+        A[n]=A[n]%B[n];
+        return resid_pol(A,B,n-1);
+    }
+}
